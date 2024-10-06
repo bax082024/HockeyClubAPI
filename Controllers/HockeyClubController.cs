85e1464cebb0345927e88e55b4c01ac8b54dc541
bax082024 <bax082024@gmail.com>
2024-10-06 12:22:51 +0200

    added admin

--- Controllers/HockeyClubController.cs
+++ Controllers/HockeyClubController.cs
@@ -6,7 +6,24 @@ using Microsoft.AspNetCore.Mvc;
 
 public class HockeyClubController : ControllerBase
 {
-  // only Admin can acces this
+  // only Admin has full access
+  [Authorize(Roles = "Admin")]
+  [HttpGet("Admin")]
+  public IActionResult GetAdminData()
+  {
+    return Ok("Admin: Full access to the system and server");
+  }
+
+  // Leader has access to all system imformation except server code.
+  [Authorize(Roles = "Leader")]
+  [HttpGet("Leader")]
+
+  public IActionResult GetLeaderData()
+  {
+    return Ok("Leader: Leader has access to all system imformation except server code.")
+  }
+
+  // Office has access to accounting and administration documents.
 
 
 }
