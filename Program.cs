6ef666825d241a1a775dcf29601ce7b8228cbf51
bax082024 <bax082024@gmail.com>
2024-10-06 11:27:10 +0200

    added roles

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,4 +30,20 @@ using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
     await CreateRoles(services);
+}
+
+async Task SeedUsers(IServiceProvider serviceProvider)
+{   
+    // Email, Password, Role/username
+    var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+    
+    var users = new List<(string Email, string Password, string Role)>
+    {
+        ("admin@hockey.com", "Admin123", "Admin"),
+        ("leader@hockey.com", "Leader123", "Leader"),
+        ("office@hockey.com", "office123", "office"),
+        ("trainer@hockey.com", "Trainer123", "Trainer"),
+        ("helper@hockey.com", "Helper123", "Helper")
+
+    };
 }
