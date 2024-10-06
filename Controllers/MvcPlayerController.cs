32e2e8517c895ca5da732d0f99b6e72f9759100e
bax082024 <bax082024@gmail.com>
2024-10-06 15:29:24 +0200

    updating mvc

--- Controllers/MvcPlayerController.cs
+++ Controllers/MvcPlayerController.cs
@@ -12,6 +12,28 @@ namespace HockeyClubAPI.MvcControllers
     {
       _context = context;
     }
+
+    // GET: Player
+    public async Task<IActionResult> Index()
+    {
+      var players = await _context.Players.ToListAsync();
+      return View(players);
+    }
+
+    // GET: Player/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+
+    public async Task<IActionResult> Create([Bind("Fullname,Position,JerseyNumber,DateOfBirth")] Player player)
+    {
+      if (ModelState.IsValid)
+      {
+        _context.Add(player);
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+      }
+    }
+
   }
 
 
