da09d0739033a94d1ac144b67e528922c7d47cb3
bax082024 <bax082024@gmail.com>
2024-10-06 10:42:23 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+async Task CreateRoles(IServiceProvider serviceProvider)
+{
+    
+
+}
