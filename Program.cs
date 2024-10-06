23e9d5aa4756ae8649fdb95da559b54864a2e302
bax082024 <bax082024@gmail.com>
2024-10-06 14:48:56 +0200

    added routing

--- Program.cs
+++ Program.cs
@@ -34,6 +34,8 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+app.UseRouting();
+
 app.UseAuthentication(); // Enable authentication
 app.UseAuthorization(); // Enable auhorization
 
